631e8a22bd554e98038875c368e5728d4c82bc4d
Slava Avramenok <_AVA@outlook.com>
2019-05-04 21:54:10 +0300

    remove redundant field in user entity

--- src/main/java/ava/db/entity/UserEntity.java
+++ src/main/java/ava/db/entity/UserEntity.java
@@ -1,21 +1,10 @@
 package ava.db.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import java.util.List;
+import javax.persistence.*;
 
 @Data
 @Builder
@@ -32,6 +21,4 @@ public class UserEntity {
     String nickname;
     @JsonIgnore
     String password;
-    @OneToMany(orphanRemoval = true, cascade = CascadeType.ALL, mappedBy = "user")
-    List<FileEntity> files;
 }
