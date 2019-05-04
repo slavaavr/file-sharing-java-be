e0af824aaaddd78ca786e7535ca341f5d550e091
Slava Avramenok <_AVA@outlook.com>
2019-05-04 17:15:25 +0300

    Add service with ability to create and get popular tags

---
+++ src/main/java/ava/service/TagService.java
@@ -0,0 +1,34 @@
+package ava.service;
+
+import ava.db.entity.TagEntity;
+import ava.db.repository.TagRepository;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class TagService {
+
+    TagRepository tagRepository;
+
+    public List<String> getPopularTags() {
+        return tagRepository.getPopularTags();
+    }
+
+    @Transactional
+    public List<TagEntity> createTags(List<String> tags) {
+        if (tags.size() == 0) {
+            return new ArrayList<>();
+        }
+        tagRepository.saveAllIfNotExist(tags);
+        return tagRepository.findAllByTitleIsIn(tags);
+    }
+}
