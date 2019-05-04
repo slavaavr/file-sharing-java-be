64fa9faa807bfdbd29d74473c69d130123fec12d
Slava Avramenok <_AVA@outlook.com>
2019-05-04 17:10:59 +0300

    improve db schema

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -4,42 +4,43 @@ CREATE TABLE IF NOT EXISTS userr
   email    VARCHAR(50) NOT NULL UNIQUE,
   nickname VARCHAR(50) NOT NULL,
   password CHAR(60)    NOT NULL
-  );
+);
 
 CREATE TABLE IF NOT EXISTS file
 (
-  id                 serial PRIMARY KEY,
-  user_id            INTEGER     NULL REFERENCES userr (id) ON DELETE CASCADE,
-  title              VARCHAR(60) NOT NULL,
-  body               BYTEA       NOT NULL,
-  file_type          VARCHAR(50) NOT NULL,
-  file_size          VARCHAR(10) NOT NULL,
-  file_storage_time  INTERVAL    NOT NULL,
-  file_creation_date TIMESTAMP            DEFAULT NOW(),
-  count_download     SMALLINT             DEFAULT 0
-  );
+  id             serial PRIMARY KEY,
+  user_id        INTEGER     NULL REFERENCES userr (id) ON DELETE CASCADE,
+  uri            VARCHAR(60) NOT NULL UNIQUE,
+  title          VARCHAR(60) NOT NULL,
+  body           BYTEA       NOT NULL,
+  type           VARCHAR(50) NOT NULL,
+  size           BIGINT      NOT NULL,
+  storage_time   VARCHAR(50) NOT NULL,
+  creation_date  TIMESTAMP DEFAULT NOW(),
+  count_download SMALLINT  DEFAULT 0
+);
 
 CREATE TABLE IF NOT EXISTS tag
 (
   id          serial PRIMARY KEY,
-  name        VARCHAR(30) NOT NULL,
+  title       VARCHAR(30) UNIQUE NOT NULL,
   use_counter INTEGER DEFAULT 0
-  );
+);
 
 CREATE TABLE IF NOT EXISTS file_tag
 (
   file_id INTEGER REFERENCES file (id) ON DELETE CASCADE,
   tag_id  INTEGER REFERENCES tag (id),
   CONSTRAINT file_tag_unique UNIQUE (file_id, tag_id)
-  );
+);
 
 CREATE PROCEDURE erase_old_files()
-LANGUAGE plpgsql
+  LANGUAGE plpgsql
 AS
 $$
 BEGIN
   DELETE
   FROM file
-  WHERE file_date < NOW() - file_lifetime;
+  WHERE creation_date > NOW() - storage_time;
 END;
 $$;
