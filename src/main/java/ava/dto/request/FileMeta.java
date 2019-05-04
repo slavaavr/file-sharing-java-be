de403dfd90d263bd1a5553de478bca73ffb57301
Slava Avramenok <_AVA@outlook.com>
2019-05-04 17:18:21 +0300

    data class to store meta file info

---
+++ src/main/java/ava/dto/request/FileMeta.java
@@ -0,0 +1,14 @@
+package ava.dto.request;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class FileMeta {
+    List<String> tags;
+    String time;
+}
