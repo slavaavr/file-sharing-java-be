8d3b2ce94a94c8603069bd61a690382a1b1d577b
Slava Avramenok <_AVA@outlook.com>
2019-05-04 17:14:43 +0300

    Add service for create and get user

---
+++ src/main/java/ava/service/UserService.java
@@ -0,0 +1,25 @@
+package ava.service;
+
+import ava.db.entity.UserEntity;
+import ava.db.repository.UserRepository;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class UserService {
+
+    UserRepository userRepository;
+
+    UserEntity getUserByEmail(String email) {
+        return userRepository.findOneByEmail(email);
+    }
+
+    UserEntity createUser(UserEntity user) {
+        return userRepository.save(user);
+    }
+}
