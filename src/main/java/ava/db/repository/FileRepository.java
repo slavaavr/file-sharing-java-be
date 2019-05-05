4d5694cbc8e1fa430f51a7ef2dd7db4d944505af
Slava Avramenok <_AVA@outlook.com>
2019-05-05 20:47:56 +0300

    enable pagination

--- src/main/java/ava/db/repository/FileRepository.java
+++ src/main/java/ava/db/repository/FileRepository.java
@@ -1,8 +1,8 @@
 package ava.db.repository;
 
 import ava.db.entity.FileEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface FileRepository extends CrudRepository<FileEntity, Integer> {
+public interface FileRepository extends PagingAndSortingRepository<FileEntity, Integer> {
     FileEntity findByUri(String uri);
 }
