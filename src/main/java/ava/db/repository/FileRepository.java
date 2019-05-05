75569ff284e28b5b2b751c281f5fc29b908fe0ea
Slava Avramenok <_AVA@outlook.com>
2019-05-06 00:15:09 +0300

    add method to update file download counted

--- src/main/java/ava/db/repository/FileRepository.java
+++ src/main/java/ava/db/repository/FileRepository.java
@@ -1,8 +1,15 @@
 package ava.db.repository;
 
 import ava.db.entity.FileEntity;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface FileRepository extends PagingAndSortingRepository<FileEntity, Integer> {
     FileEntity findByUri(String uri);
+
+    @Modifying
+    @Query(value = "update file set count_download=count_download+1 where id=:id", nativeQuery = true)
+    void incrDownloadCounter(@Param("id") Integer id);
 }
