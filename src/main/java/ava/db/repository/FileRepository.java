d9e7bac885a5ff4b6279fb50c63a5c8acad6c904
Slava Avramenok <_AVA@outlook.com>
2019-05-20 00:10:01 +0300

    add search file queries, and query for invoke stored proc to erase old files

--- src/main/java/ava/db/repository/FileRepository.java
+++ src/main/java/ava/db/repository/FileRepository.java
@@ -1,15 +1,28 @@
 package ava.db.repository;
 
 import ava.db.entity.FileEntity;
+import ava.dto.responce.SimpleFile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-public interface FileRepository extends PagingAndSortingRepository<FileEntity, Integer> {
+public interface FileRepository extends CrudRepository<FileEntity, Integer> {
+
     FileEntity findByUri(String uri);
 
     @Modifying
     @Query(value = "update file set count_download=count_download+1 where id=:id", nativeQuery = true)
     void incrDownloadCounter(@Param("id") Integer id);
+
+    @Query(value = "select * from file where title ILIKE CONCAT('%',:title,'%')", nativeQuery = true)
+    Page<SimpleFile> searchFiles(@Param("title") String title, Pageable pageable);
+
+    @Modifying
+    @Query(value = "call erase_old_files()", nativeQuery = true)
+    void eraseOldFiles();
+
+    Page<SimpleFile> findAll(Pageable pageable);
 }
