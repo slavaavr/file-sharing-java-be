00bfe8bf493f9dc5511d08051972a3ef5e20e591
Slava Avramenok <_AVA@outlook.com>
2019-05-19 20:01:48 +0300

    rename user table, add pretty_size field to file entity

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,41 +1,42 @@
-CREATE TABLE IF NOT EXISTS userr
+CREATE TABLE IF NOT EXISTS usr
 (
-  id       serial PRIMARY KEY,
-  email    VARCHAR(50) NOT NULL UNIQUE,
-  nickname VARCHAR(50) NOT NULL,
-  password CHAR(60)    NOT NULL
+    id       serial PRIMARY KEY,
+    email    VARCHAR(50) NOT NULL UNIQUE,
+    nickname VARCHAR(50) NOT NULL,
+    password CHAR(60)    NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS file
 (
-  id             serial PRIMARY KEY,
-  user_id        INTEGER     NULL REFERENCES userr (id) ON DELETE CASCADE,
-  uri            VARCHAR(60) NOT NULL UNIQUE,
-  title          VARCHAR(60) NOT NULL,
-  body           BYTEA       NOT NULL,
-  type           VARCHAR(50) NOT NULL,
-  size           BIGINT      NOT NULL,
-  storage_time   VARCHAR(50) NOT NULL,
-  creation_date  TIMESTAMP DEFAULT NOW(),
-  count_download SMALLINT  DEFAULT 0
+    id             serial PRIMARY KEY,
+    user_id        INTEGER     NULL REFERENCES usr (id) ON DELETE CASCADE,
+    uri            VARCHAR(60) NOT NULL UNIQUE,
+    title          VARCHAR(60) NOT NULL,
+    body           BYTEA       NOT NULL,
+    type           VARCHAR(50) NOT NULL,
+    size           BIGINT      NOT NULL,
+    pretty_size    VARCHAR(10) NOT NULL,
+    storage_time   VARCHAR(50) NOT NULL,
+    creation_date  TIMESTAMP DEFAULT NOW(),
+    count_download SMALLINT  DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS tag
 (
-  id          serial PRIMARY KEY,
-  title       VARCHAR(30) UNIQUE NOT NULL,
-  use_counter INTEGER DEFAULT 0
+    id          serial PRIMARY KEY,
+    title       VARCHAR(30) UNIQUE NOT NULL,
+    use_counter INTEGER DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS file_tag
 (
-  file_id INTEGER REFERENCES file (id) ON DELETE CASCADE,
-  tag_id  INTEGER REFERENCES tag (id),
-  CONSTRAINT file_tag_unique UNIQUE (file_id, tag_id)
+    file_id INTEGER REFERENCES file (id) ON DELETE CASCADE,
+    tag_id  INTEGER REFERENCES tag (id),
+    CONSTRAINT file_tag_unique UNIQUE (file_id, tag_id)
 );
 
 CREATE PROCEDURE erase_old_files()
-  LANGUAGE plpgsql
+    LANGUAGE plpgsql
 AS
 $$
 BEGIN
